3f369bef672e309143932bd0a2f05571ab93e5d6
Ju0011 <yhs07240@naver.com>
2023-04-14 16:51:03 +0900

    values

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,21 +1,69 @@
-import React from 'react'
-
+/*eslint-disable*/
+import React, {useState} from 'react';
 import Input from 'components/Input';
 import './SignUp.css';
 
+
+
 const SignUp = () => {
+
+   
+
+/*
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        repassword: "",
+        name: "",
+        age: ""
+    })
+    */
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [repassword, setRepassword] = useState("")
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+
+    function BtnCheck() {
+        alert({ name} + "님 환영합니다!");
+    }
+
+    const [errors, setErrors] = useState({
+        password: ""
+    })
+
+
+    const HandleChange = (e) => {
+        /*
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value,
+        })*/
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault() //submit 관련 고유 동작 중단시키기
+        console.log(JSON.stringify(values, null, 2))
+        
+    }
+
     return (
         <div className="form-wrapper">
             <h1>회원가입</h1>
-            <form className="form">
-                <Input title="이메일" type="email" required></Input>
-                <Input title="비밀번호" type="password" required></Input>
-                <Input title="비밀번호 재확인" type="password" required></Input>
-                <Input title="이름" type="text"></Input>
-                <Input title="나이" type="number"></Input>
+            <form className="form" onSubmit={handleSubmit}>
+                
+                <Input title="이메일" type="email" required onChange={HandleChange} value={values.email}></Input>
+                <Input title="비밀번호" type="password" required onChange={HandleChange} value={values.password}></Input>
+                <Input title="비밀번호 재확인" type="password" required onChange={HandleChange} value={values.repassword}></Input>
+                <Input title="이름" type="text" onChange={HandleChange} value={values.name}></Input>
+                <Input title="나이" type="number" onChange={HandleChange} value={values.age}></Input>
+            
+
                 <div
                     style={{ marginTop: "30px" }}>
-                    <button className="btn" type="submit">가입하기</button>
+                    <button className="btn" type="submit" onSubmit={handleSubmit} onClick={BtnCheck}>가입하기</button>
                 </div>
             </form>
         </div>
